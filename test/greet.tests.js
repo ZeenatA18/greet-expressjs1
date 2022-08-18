b2f3586c6e39f55c2f40c0790dc5a9e5eb0588ef
Zeenat <zeenatavontuur@gmail.com>
2022-08-18 13:06:52 +0200

    null ssl

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -5,10 +5,7 @@ const pgp = require('pg-promise')();
 const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://postgres:pg123@localhost:5432/greetings_test';
 
 const config = {
-    connectionString: DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    connectionString: DATABASE_URL
 }
 
 const db = pgp(config);
